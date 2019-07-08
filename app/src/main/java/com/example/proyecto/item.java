594a702b7bf805c1ceb9afda8482ef2a94d0ddc2
Yessica <yessnicolerp@gmail.com>
2019-07-08 02:30:10 -0400

    camara y galeria

---
+++ app/src/main/java/com/example/proyecto/item.java
@@ -0,0 +1,13 @@
+package com.example.proyecto;
+
+public class item {
+    private String url;
+    item (String u )
+    {
+        url=u;
+    }
+    public String getUrl()
+    {
+        return url;
+    }
+}
--- app/src/main/java/com/example/proyecto/producto.java
+++ app/src/main/java/com/example/proyecto/producto.java
@@ -1,12 +1,18 @@
 package com.example.proyecto;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,25 +22,30 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
-
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
-
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class producto extends AppCompatActivity implements View.OnClickListener {
-    ArrayList<String> categoria;
 
+        //declarar variables
     Button btn;
+    ArrayList<String> categoria;
     ImageView img;
     Intent i;
     Bitmap bmp;
+    Adapter_menu men;
+    //RecyclerView recycler;
+    GridLayoutManager lay;
+   private final int CODE_PERMISSIONS = 101;
     final static int cons = 0;
 
     @Override
@@ -54,20 +65,41 @@ public class producto extends AppCompatActivity implements View.OnClickListener 
         });*/
         setSpinner();
         loadComponents();
-        init();
+    //empezamos con la configuracion de los adaptadores Adapter_menu, item
+       // recycler = findViewById(R.id.recyclerId);
+        //cargar();
     }
-
-    public void init(){
+    //recuperamos
+    private void loadComponents() {
+        Button btnregistrarpro = findViewById(R.id.btnregistropro);
+        btnregistrarpro.setOnClickListener(this);
+        //parte de camara
         btn = (Button)findViewById(R.id.subimg);
         btn.setOnClickListener(this);
         img = (ImageView)findViewById(R.id.subim);
     }
 
-    private void loadComponents() {
-        Button btnregistrarpro = findViewById(R.id.btnregistropro);
-        btnregistrarpro.setOnClickListener(this);
-
-    }
+    //cargar imagen
+    /*
+    private void cargar() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(Utils.HOST+"image",null,new JsonHttpResponseHandler(){
+            @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response){
+                        super.onSuccess(statusCode , headers,response);
+                        for(int i=0;i<response.length();i++){
+                            try {
+                                JSONObject dat = response.getJSONObject(i);
+                                String url = dat.getString("url");
+                                men.add(new item(url));
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+                        }
+            }
+        });
+    }*/
+    //hasta aqui cargar imagen
 
     @Override
     public void onClick(View v) {
@@ -78,8 +110,7 @@ public class producto extends AppCompatActivity implements View.OnClickListener 
                 break;
             }
             case R.id.subimg:{
-                i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i,cons);
+                cargarImagen();
                 break;
             }
         }
@@ -101,6 +132,8 @@ public class producto extends AppCompatActivity implements View.OnClickListener 
         EditText stocks = findViewById(R.id.stock);
         EditText descripciones = findViewById(R.id.descripcion);
         Spinner cats = findViewById(R.id.btncategoria);
+        ImageView imagen = findViewById(R.id.subim);
+
 
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
@@ -122,6 +155,70 @@ public class producto extends AppCompatActivity implements View.OnClickListener 
             }
         });
     }
+
+    //elegir o galeria o camara
+
+    //opciones para acceder a la camara o galeria
+    //declaramos variables
+    final int COD_GALERIA=10;
+    final int COD_CAMERA=20;
+    String path;
+    private void cargarImagen() {
+        final CharSequence[] opciones={"Tomar Foto","Cargar Imagen","Cancelar"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(producto.this);
+        alertOpciones.setTitle("Seleccione una Opción");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")){
+                    tomarFotografia();
+                }else{
+                    if (opciones[i].equals("Cargar Imagen")){
+                        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),COD_GALERIA);
+                    }else{
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        alertOpciones.show();
+    }
+
+    private void tomarFotografia() {
+
+    }
+
+    //enviar Datos
+    /*
+    private void enviarDatos(){
+        if (path == null || path == ""){
+            Toast.makeText(this,"cargar imagen");
+            return;
+        }
+        File file = new File (path);
+        try {
+            params.put("img",file);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(),path,Toast.LENGTH_SHORT).show();
+        client.post(Utils.HOST+"image",params,(JsonHttpResponseHandler) super.onSuccess(statusCode,headers,response);
+        try {
+            String mesagge = response.getString ("menssage")
+            if (mesagge!= null) {
+                Toast.makeText(getApplicationContext(),mesagge,Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        );
+
+    }
+*/
+    //hasta aqui datos enviados
 
     private void  setSpinner(){ //Adaptador para la categoría
         categoria = new ArrayList<>();
